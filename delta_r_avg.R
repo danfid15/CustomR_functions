f54aea337cf44047176c94dfbdeb8d203a6a8a8f
danfid15 <danielfidalgo15@gmail.com>
2022-03-07 20:10:20 +0200

    updated delta_r_avg

--- delta_r_avg.R
+++ delta_r_avg.R
@@ -5,12 +5,13 @@ delta_r_avg <- function(delta_r,delta_error){
 
 library(tidyverse)
   
-data <- data.frame(cbind(delta_r = delta_r,delta_error = delta_error))
+data <- data.frame(cbind(delta_r = delta_r, delta_error = delta_error))
 
 summary_delta <- data%>%
-  select(delta_r,delta_error)%>%
-  summarise(weighted_mean = round(sum(delta_r)/sum(delta_error),0),
-            uncertainty = 1/sum(delta_error))
+  mutate(delta_r_var = delta_r/delta_error^2,
+         delta_error_var = 1/delta_error^2) %>%
+  summarise(weighted_mean = round(sum(delta_r_var)/sum(delta_error_var),0),
+            uncertainty = 1/sum(delta_error_var))
 
 return(summary_delta)
 
